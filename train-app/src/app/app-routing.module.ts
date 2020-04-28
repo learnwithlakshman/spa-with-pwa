3237f8dda9fb42bda8bc824518d2ae1d7b500e0d
M Rajesh <m.rajeshrajz@gmail.com>
2020-04-28 12:31:23 +0530

    added pages in train-app

--- train-app/src/app/app-routing.module.ts
+++ train-app/src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
+import { MybookingsComponent } from './pages/mybookings/mybookings.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full',
+    redirectTo: 'app/home',
+  },
+  {
+    path: 'app',
+    component: AppComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'mybookings',
+        component: MybookingsComponent
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
