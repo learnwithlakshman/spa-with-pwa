8791a8d4746210b8971b075952e8e8d279663d78
M Rajesh <m.rajeshrajz@gmail.com>
2020-04-28 16:00:09 +0530

    bug fixed and completed train and bus app

--- train-app/src/app/app-routing.module.ts
+++ train-app/src/app/app-routing.module.ts
@@ -1,25 +1,24 @@
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { MybookingsComponent } from './pages/mybookings/mybookings.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+
   {
-    path: 'app',
-    component: AppComponent,
-    children: [
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: 'mybookings',
-        component: MybookingsComponent
-      }
-    ]
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'mybookings',
+    component: MybookingsComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   },
   {
     path: '**', component: EmptyRouteComponent
